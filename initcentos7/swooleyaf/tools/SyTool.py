292cd477fdf22dee6d0c8ca7e23b16e333c7c3a8
jiangwei <jiangwei07061625@163.com>
2018-09-14 15:50:54 +0800

    php version update to 7.1.22 and 7.2.10

--- initcentos7/swooleyaf/tools/SyTool.py
+++ initcentos7/swooleyaf/tools/SyTool.py
@@ -292,8 +292,8 @@ class SyTool():
     def installPhp7():
         run('yum -y install libxslt libxml2 libxml2-devel mysql-devel openldap openldap-devel gmp-devel')
 
-        php7Local = ''.join([syDicts['path.package.local'], '/resources/php7/php-7.1.21.tar.gz'])
-        php7Remote = ''.join([syDicts['path.package.remote'], '/php-7.1.21.tar.gz'])
+        php7Local = ''.join([syDicts['path.package.local'], '/resources/php7/php-7.1.22.tar.gz'])
+        php7Remote = ''.join([syDicts['path.package.remote'], '/php-7.1.22.tar.gz'])
         put(php7Local, php7Remote)
         with cd(syDicts['path.package.remote']):
             run('mkdir /tmp/swooleyaf')
@@ -302,8 +302,8 @@ class SyTool():
             run('mkdir /home/logs/seaslog-cli')
             run('mkdir /home/logs/seaslog-fpm')
             run('mkdir /usr/local/php7')
-            run('tar -zxvf php-7.1.21.tar.gz')
-            run('cd php-7.1.21/ && ./configure --prefix=/usr/local/php7 --exec-prefix=/usr/local/php7 --bindir=/usr/local/php7/bin --sbindir=/usr/local/php7/sbin --includedir=/usr/local/php7/include --libdir=/usr/local/php7/lib/php --mandir=/usr/local/php7/php/man --with-config-file-path=/usr/local/php7/etc --with-mysql-sock=/usr/local/mysql/mysql.sock --with-zlib=/usr/local/zlib --with-mhash --with-openssl=/usr/local/openssl --with-mysqli=shared,mysqlnd --with-pdo-mysql=shared,mysqlnd --with-iconv --enable-zip --enable-inline-optimization --disable-debug --disable-rpath --enable-shared --enable-xml --enable-pcntl --enable-bcmath --enable-mysqlnd --enable-sysvsem --with-mysqli --enable-embedded-mysqli  --with-pdo-mysql --enable-shmop --enable-mbregex --enable-mbstring --enable-ftp --enable-sockets --with-xmlrpc --enable-soap --without-pear --with-gettext --enable-session --with-curl --enable-opcache --enable-fpm --without-gdbm --enable-fileinfo --with-gmp && make && make install')
+            run('tar -zxvf php-7.1.22.tar.gz')
+            run('cd php-7.1.22/ && ./configure --prefix=/usr/local/php7 --exec-prefix=/usr/local/php7 --bindir=/usr/local/php7/bin --sbindir=/usr/local/php7/sbin --includedir=/usr/local/php7/include --libdir=/usr/local/php7/lib/php --mandir=/usr/local/php7/php/man --with-config-file-path=/usr/local/php7/etc --with-mysql-sock=/usr/local/mysql/mysql.sock --with-zlib=/usr/local/zlib --with-mhash --with-openssl=/usr/local/openssl --with-mysqli=shared,mysqlnd --with-pdo-mysql=shared,mysqlnd --with-iconv --enable-zip --enable-inline-optimization --disable-debug --disable-rpath --enable-shared --enable-xml --enable-pcntl --enable-bcmath --enable-mysqlnd --enable-sysvsem --with-mysqli --enable-embedded-mysqli  --with-pdo-mysql --enable-shmop --enable-mbregex --enable-mbstring --enable-ftp --enable-sockets --with-xmlrpc --enable-soap --without-pear --with-gettext --enable-session --with-curl --enable-opcache --enable-fpm --without-gdbm --enable-fileinfo --with-gmp && make && make install')
 
         php7CliIniLocal = ''.join([syDicts['path.package.local'], '/configs/swooleyaf/php7/php-cli.ini'])
         php7CliIniRemote = '/usr/local/php7/etc/php-cli.ini'
@@ -320,7 +320,7 @@ class SyTool():
         run('chmod 754 /lib/systemd/system/php7-fpm.service')
         run('systemctl enable php7-fpm.service')
 
-        php7DirRemote = ''.join([syDicts['path.package.remote'], '/php-7.1.21'])
+        php7DirRemote = ''.join([syDicts['path.package.remote'], '/php-7.1.22'])
         with cd(php7DirRemote):
             run('groupadd www')
             run('useradd -g www www -s /sbin/nologin')
